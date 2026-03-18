205c5be8ad2b1f9c51d3a8fa28f868cdc1ecb09a
milypferraresso <mily.p.ferraresso@gmail.com>
2026-03-18 13:22:37 -0300

    Meu primeiro commit

---
+++ condicao-complexa-AND/script.js
@@ -0,0 +1,8 @@
+let idade = Number(prompt("Digite sua idade:"));
+let tem CNH = prompt("Possui CNH? (sim ou nao");
+
+if (idade >= 18 && tem CNH === "sim") { 
+    alert("Pode dirigir");
+} else {
+        alert("Não pode dirigir");
+}
