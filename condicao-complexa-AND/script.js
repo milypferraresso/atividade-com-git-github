2a2e383e586da7248ebde6811e96686e484ef840
milypferraresso <mily.p.ferraresso@gmail.com>
2026-03-25 09:13:10 -0300

    Condicional utilizando o operador OR/OU

--- condicao-complexa-AND/script.js
+++ condicao-complexa-AND/script.js
@@ -1,5 +1,5 @@
 let idade = Number(prompt("Digite sua idade:"));
-let tem CNH = prompt("Possui CNH? (sim ou nao");
+let  = prompt("Possui CNH? (sim ou nao");
 
 if (idade >= 18 && tem CNH === "sim") { 
     alert("Pode dirigir");
